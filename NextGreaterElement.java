6ff0aad01bf1a81227652f92a3f4db61bd806484
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-10-24 23:44:08 +0530

    Next Greater Element

---
+++ NextGreaterElement.java
@@ -0,0 +1,24 @@
+public class NextGreaterElement {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int[] res = new int[n1];
+        
+         
+        for(int i = 0; i < n1; i++){
+            int next = -1;
+            boolean found = false; // martlab initially element mil gya h ya nhi 
+            for(int j = 0; j < n2; j++){
+                if(nums2[j] == nums1[i]){
+                    found = true; // start checking next greater after this
+                }
+                else if(found && nums2[j] > nums1[i]){
+                    next = nums2[j];
+                    break;
+                }
+            }
+            res[i] = next;
+        }
+        return res;
+    }
+}
